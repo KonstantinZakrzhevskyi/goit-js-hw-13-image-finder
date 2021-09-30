c5e1843d14541691e0d063e5cdbd9d3353790f77
KonstantinZakrzhevskyi <konstantinz2505@gmail.com>
2021-09-30 23:00:24 +0300

    Добавляет запрос apiService

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,24 +1,19 @@
-// const BASE_URL = 'ttps://pixabay.com/api';
-// const API_KEY = '23616749-9d77515f343e9cfce3b12231a';
+const BASE_URL = 'https://pixabay.com/api';
+const API_KEY = '23616749-9d77515f343e9cfce3b12231a';
 
-// const params = '?image_type=photo&orientation=horizontal';
-// const searchQuery = '';
-// const pageNumber = 1;
-// const perPage = 12;
-
-
-
-// function featchGallery() {
-//  const url = (`${BASE_URL}/${params}&q=${searchQuery}&page=${pageNumber}&per_page=${perPage}&key=${API_KEY}`);
-
-//   return fetch(url)
-// }
-
-// export default { featchGallery };
+const params = 'image_type=photo&orientation=horizontal';
+const query = '';
+const pageNumber = 1;
+const perPage = 12;
 
 
-// const r = ауеср('https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_car&page=1&per_page=12&key=23616749-9d77515f343e9cfce3b12231a')
-// console.log(r);
+
+function fetchGallery(query) {
+  const url = (`${BASE_URL}/?${params}&q=${query}&page=${pageNumber}&per_page=${perPage}&key=${API_KEY}`);
+  console.log(url);
+
+  return fetch(url).then(response => response.json());
+}
 
 
 
